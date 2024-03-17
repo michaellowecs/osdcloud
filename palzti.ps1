6846c177661d413369e1a505f5a488cd820f12a1
michaellowecs <113188695+michaellowecs@users.noreply.github.com>
2024-03-17 19:49:31 +0100

    Update palzti.ps1

--- palzti.ps1
+++ palzti.ps1
@@ -27,12 +27,14 @@ $Global:MyOSDCloud = @{
 
 $Params = @{
     OSVersion = "Windows 10"
-    OSBuild = "21H2"
-    OSEdition = "Pro"
-    OSLanguage = "en-us"
+    OSBuild = "22H2"
+    OSEdition = "Enterprise"
+    OSLanguage = "de-de"
     Firmware = $false
     ZTI = $true
 }
+$Product = Get-MyComputerProduct
+$DriverPack = Get-OSDCloudDriverPacks | Where-Object {($_.Product -contains $Product) -and ($_.OS -match $Params.OSVersion)}
 Start-OSDCloud @Params
 
 #================================================
@@ -56,7 +58,6 @@ $OOBEDeployJson = @'
                         "Microsoft.People",
                         "Microsoft.SkypeApp",
                         "Microsoft.Wallet",
-                        "Microsoft.WindowsCamera",
                         "microsoft.windowscommunicationsapps",
                         "Microsoft.WindowsFeedbackHub",
                         "Microsoft.WindowsMaps",
@@ -84,79 +85,17 @@ If (!(Test-Path "C:\ProgramData\OSDeploy")) {
 $OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
 
 #================================================
-#  [PostOS] AutopilotOOBE Configuration Staging
-#================================================
-Write-Host -ForegroundColor Green "Create C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json"
-$AutopilotOOBEJson = @'
-{
-    "Assign":  {
-                   "IsPresent":  true
-               },
-    "GroupTag":  "GroupTagXXX",
-    "AddToGroup": "AADGroupX",
-    "AddToGroupOptions":  [
-                    "AADGroupX",
-                    "AADroupY"
-    ],
-    "Hidden":  [
-                   "AssignedComputerName",
-                   "AssignedUser",
-                   "PostAction",
-                   "GroupTag",
-                   "Assign"
-               ],
-    "PostAction":  "Quit",
-    "Run":  "NetworkingWireless",
-    "Docs":  "https://google.com/",
-    "Title":  "AkosCloud Autopilot Register"
-}
-'@
-If (!(Test-Path "C:\ProgramData\OSDeploy")) {
-    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
-}
-$AutopilotOOBEJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.AutopilotOOBE.json" -Encoding ascii -Force
-
-#================================================
 #  [PostOS] AutopilotOOBE CMD Command Line
 #================================================
 Write-Host -ForegroundColor Green "Create C:\Windows\System32\OOBEAkos.cmd"
 $OOBECMD = @'
 PowerShell -NoL -Com Set-ExecutionPolicy RemoteSigned -Force
 Set Path = %PATH%;C:\Program Files\WindowsPowerShell\Scripts
-Start /Wait PowerShell -NoL -C Install-Module AutopilotOOBE -Force -Verbose
-Start /Wait PowerShell -NoL -C Install-Module OSD -Force -Verbose
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Set-KeyboardLanguage.ps1
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://raw.githubusercontent.com/AkosBakos/OSDCloud/main/Install-EmbeddedProductKey.ps1
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://check-autopilotprereq.osdcloud.ch
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://start-autopilotoobe.osdcloud.ch
-Start /Wait PowerShell -NoL -C Start-OOBEDeploy
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://tpm.osdcloud.ch
-Start /Wait PowerShell -NoL -C Invoke-WebPSScript https://cleanup.osdcloud.ch
+Start /Wait PowerShell -NoL -C Install-Language en-us
+Start /Wait PowerShell -NoL -C Install-Language fr-fr
 Start /Wait PowerShell -NoL -C Restart-Computer -Force
 '@
 $OOBECMD | Out-File -FilePath 'C:\Windows\System32\OOBEAkos.cmd' -Encoding ascii -Force
-
-#================================================
-#  [PostOS] SetupComplete CMD Command Line
-#================================================
-Write-Host -ForegroundColor Green "Create C:\Windows\Setup\Scripts\SetupComplete.cmd"
-$SetupCompleteCMD = @'
-'@
-$SetupCompleteCMD | Out-File -FilePath 'C:\Windows\Setup\Scripts\SetupComplete.cmd' -Encoding ascii -Force
-
-#================================================
-#  [PostOS] Installing August CU for Autopilot HW hash issues
-#================================================
-If ($Params.OSVersion -eq "Windows 10" -and $Params.OSBuild -eq "21H2") {
-    Write-Host -ForegroundColor Cyan "Installing CU for Autopilot HW hash issues"
-    Invoke-Expression (Invoke-RestMethod https://cu.osdcloud.ch)
-
-    Write-Host -ForegroundColor Gray "Download August CU PPKG from Azure Blob Storage"
-    Invoke-Expression "& curl.exe --insecure --location --output 'C:\OSDCloud\Packages\Install_CU.ppkg' --url 'https://XXXX.blob.core.windows.net/packages/Install_CU.ppkg'"
-    
-    Write-Host -ForegroundColor Gray "Importing August CU as PPKG"
-    DISM.exe /Image:C:\ /Add-ProvisioningPackage /PackagePath:C:\OSDCloud\Packages\Install_CU.ppkg
-}
 
 #=======================================================================
 #   Restart-Computer
